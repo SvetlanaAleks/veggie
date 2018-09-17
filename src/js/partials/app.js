8c0684632fa38ffc97674def786e84e33038bb27
Svetlana Alexeyenko <sv.alexeyenko@gmail.com>
2018-09-17 23:29:41 +0300

    add descktop

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,6 +1,22 @@
 //Some JS
-let a='Hello';
-let b='World';
-[a, b] = [b, a];
+$(document).ready(function () {
+   var tabLinks = $('.js_tab-link');
+   var menuList = $('.js_menu-list');
+   tabLinks.click(function (e) {
+      e.preventDefault();
+      var $this = $(this);
+      var menuTab = $this.parents('.menu__tabs');
+      var menuItems = menuTab.find('.list');
+      var parent = $this.parents('.list');
+      menuItems.removeClass('list--active');
+      parent.addClass('list--active');
 
-console.log(`${a} ${b}`);
+      var target = $this.attr('href');
+      var targetTab = $(target);
+
+      menuList.removeClass('menu__list--active');
+      targetTab.addClass('menu__list--active');
+
+
+   });
+});
